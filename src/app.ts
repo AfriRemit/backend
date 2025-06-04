d76eba9083221c0139e55270f8633cdeb76b3b2f
alex njoya <njoyaalexander71@gmail.com>
2025-06-04 01:30:01 +0000

    expres

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/route';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+export default app;
